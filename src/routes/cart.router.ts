b5166a8fe219e94a119d4ba39f4a4457d6966e6e
mariegautron <marigaut2@gmail.com>
2021-11-05 11:56:15 +0100

    not finished but too bad

--- src/routes/cart.router.ts
+++ src/routes/cart.router.ts
@@ -1,21 +1,18 @@
-import { Router } from 'express';
-import { CartsService } from '../services/carts.service';
-
+import { Router } from "express";
+import { CartsService } from "../services/carts.service";
 
 const cartRouter = Router();
 
 const cartService = new CartsService();
 
-
-/**
- * @openapi
- * /cart:
- *   add item to user's cart
- */
- cartRouter.get('/:bookId', (req, res) => {
-    const book = cartService.addBookToCart(req.params.bookId)
-    res.status(200).send(book);
-})
-
+// /**
+//  * @openapi
+//  * /cart:
+//  *   add item to user's cart
+//  */
+//  cartRouter.get('/:bookId', (req, res) => {
+//     const book = cartService.addBookToCart(req.params.bookId)
+//     res.status(200).send(book);
+// })
 
 export default cartRouter;
--- src/services/carts.service.ts
+++ src/services/carts.service.ts
@@ -1,17 +1,13 @@
-import { CartDao } from '../dao/cart.dao';
-import { UserModel } from '../models/user.model';
+import { CartDao } from "../dao/cart.dao";
+import { UserModel } from "../models/user.model";
 
-const uuid = require('uuid')
-const jwt = require('jsonwebtoken');
+const uuid = require("uuid");
+const jwt = require("jsonwebtoken");
 
 export class CartsService {
-    private cartsDAO: CartDao = new CartDao()
+  private cartsDAO: CartDao = new CartDao();
 
-
-    public addBookToCart(bookId: string): UserModel {
-        return this.cartsDAO.addBookToCart(bookId)
-    }
-
-
-
+  // public addBookToCart(bookId: string): UserModel {
+  //     return this.cartsDAO.addBookToCart(bookId)
+  // }
 }
