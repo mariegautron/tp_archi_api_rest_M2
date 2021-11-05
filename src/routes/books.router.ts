57f6ddc74ebc11cf4d1f9c09cb7c0cf55b81c589
mariegautron <marigaut2@gmail.com>
2021-11-05 10:09:48 +0100

    post/delete user + auth

--- src/routes/books.router.ts
+++ src/routes/books.router.ts
@@ -1,7 +1,6 @@
-import { Router } from 'express';
-import { BooksService } from '../services/books.service';
-import Auth from '../auth'
-
+import { Router } from "express";
+import { BooksService } from "../services/books.service";
+import Auth from "../auth";
 
 const bookRouter = Router();
 
@@ -12,32 +11,39 @@ const booksService = new BooksService();
  * /books:
  *   getById
  */
- bookRouter.get('/:bookId', (req, res) => {
-    const book = booksService.getByID(req.params.bookId);
-    res.status(200).send(book);
-})
-
+bookRouter.get("/:bookId", (req, res) => {
+  const book = booksService.getByID(req.params.bookId);
+  res.status(200).send(book);
+});
 
 /**
  * @openapi
  * /books:
  *   getByCriteria
  */
- bookRouter.get('/', (req, res) => {
-    const books = booksService.getByCriteria(req.query as {title: string, author: string, isbn: string});
-    res.status(200).send(books);
-})
+bookRouter.get("/", (req, res) => {
+  const books = booksService.getByCriteria(
+    req.query as { title: string; author: string; isbn: string }
+  );
+  res.status(200).send(books);
+});
 
 /**
  * @openapi
  * /books:
  *   post
  */
- bookRouter.post('/:bookId/comments/', Auth.token, Auth.role('lecteur'),  (req, res) => {
-    const books = booksService.addCommentToBook(req.body.comment, req.params.bookId)
+bookRouter.post(
+  "/:bookId/comments/",
+  Auth.token,
+  Auth.role("reader"),
+  (req, res) => {
+    const books = booksService.addCommentToBook(
+      req.body.comment,
+      req.params.bookId
+    );
     res.status(200).send(books);
-})
-
-
+  }
+);
 
 export default bookRouter;
